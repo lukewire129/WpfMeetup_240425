147e3522b46354071c5b5043b6e55f29731b4652
lukewire <lukewire129@gmail.com>
2024-04-20 21:24:13 +0900

    Updates ButtonSample

---
+++ 1.Style_Themes/AvaloniaButtonSample_Theme/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace AvaloniaButtonSample_Theme
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent ();
+        }
+    }
+}
