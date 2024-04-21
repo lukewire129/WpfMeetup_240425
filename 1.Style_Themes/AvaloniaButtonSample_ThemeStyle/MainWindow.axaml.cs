f78db01d3d4be7c45adc4ebad6177fb5e402010e
lukewire <lukewire129@gmail.com>
2024-04-21 21:13:27 +0900

    Resources + Styles

---
+++ 1.Style_Themes/AvaloniaButtonSample_ThemeStyle/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace AvaloniaButtonSample_ThemeStyle;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
