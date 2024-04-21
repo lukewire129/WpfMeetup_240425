9cd910d614b82b7219915d7bc6ddfe6671d8d5b1
lukewire <lukewire129@gmail.com>
2024-04-21 21:01:51 +0900

    Android Sample

---
+++ 2.listbox/ListBoxAvalonia/ListBoxAvalonia/Views/MainView.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace ListBoxAvalonia.Views
+{
+    public partial class MainView : UserControl
+    {
+        public MainView()
+        {
+            InitializeComponent ();
+        }
+    }
+}
